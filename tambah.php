56755a77580089122d3ec01eb1fdabe574213e02
rifkiahmadsururi <rifki.sururi27@gmail.com>
2019-04-08 23:14:21 +0700

    Add files via upload

---
+++ tambah.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tambah data mahasiswa</title>
+</head>
+<body>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<form action="tambah_action.php">
+		<table class="table">
+			<tr><td>Nama</td><td><input type="text" name="nama"></td></tr>
+			<tr><td>Alamat</td><td><input type="text" name="alamat"></td></tr>
+			<tr><td>No HP</td><td><input type="text" name="no_hp"></td></tr>
+		</table>
+	</form>
+
+</body>
+</html>
