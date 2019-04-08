11d2a0598c1059668eb5a64ef9f7b279601f532d
rifkiahmadsururi <rifki.sururi27@gmail.com>
2019-04-08 23:16:27 +0700

    Add files via upload

--- tambah.php
+++ tambah.php
@@ -6,11 +6,12 @@
 <body>
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<form action="tambah_action.php">
+	<form action="tambah_action.php" method="POST">
 		<table class="table">
 			<tr><td>Nama</td><td><input type="text" name="nama"></td></tr>
 			<tr><td>Alamat</td><td><input type="text" name="alamat"></td></tr>
 			<tr><td>No HP</td><td><input type="text" name="no_hp"></td></tr>
+			<tr><td>Aksi</td><td><input type="submit" name="" value="Simpan Data"></td></tr>
 		</table>
 	</form>
 
