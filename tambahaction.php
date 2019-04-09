45b83b6b390f0f5cf537ba45c77f710264bae149
rifkiahmadsururi <rifki.sururi27@gmail.com>
2019-04-09 09:03:48 +0700

    Add files via upload

--- tambahaction.php
+++ tambahaction.php
@@ -2,11 +2,11 @@
             //include 'koneksi.php';
             $nama = $_POST['nama'];
             echo $nama;
-            // $alamat = $_POST['alamat'];
-            // $no_hp = $_POST['no_hp'];
+            $alamat = $_POST['alamat'];
+            $no_hp = $_POST['no_hp'];
             // // Insert data
-            // $sql_insert = "INSERT INTO mahasiswa (nama, alamat, no_hp) 
-            //             VALUES ($nama, $alamat, $no_hp)";
+            $sql_insert = "INSERT INTO mahasiswa (nama, alamat, no_hp) 
+                         VALUES ($nama, $alamat, $no_hp)";
             // $stmt = $conn->prepare($sql_insert);
             // $stmt->bindValue(1, $nama);
             // $stmt->bindValue(2, $alamat);
