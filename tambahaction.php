64f6d4195904dca3e61b36ca2016c09ea4bf4231
rifkiahmadsururi <rifki.sururi27@gmail.com>
2019-04-09 08:54:01 +0700

    Add files via upload

--- tambahaction.php
+++ tambahaction.php
@@ -1,17 +1,18 @@
 <?php 
-            include 'koneksi.php';
+            //include 'koneksi.php';
             $nama = $_POST['nama'];
-            $alamat = $_POST['alamat'];
-            $no_hp = $_POST['no_hp'];
-            // Insert data
-            $sql_insert = "INSERT INTO mahasiswa (nama, alamat, no_hp) 
-                        VALUES ($nama, $alamat, $no_hp)";
-            $stmt = $conn->prepare($sql_insert);
-            $stmt->bindValue(1, $nama);
-            $stmt->bindValue(2, $alamat);
-            $stmt->bindValue(3, $no_hp);
-            $stmt->execute();
+            echo $nama;
+            // $alamat = $_POST['alamat'];
+            // $no_hp = $_POST['no_hp'];
+            // // Insert data
+            // $sql_insert = "INSERT INTO mahasiswa (nama, alamat, no_hp) 
+            //             VALUES ($nama, $alamat, $no_hp)";
+            // $stmt = $conn->prepare($sql_insert);
+            // $stmt->bindValue(1, $nama);
+            // $stmt->bindValue(2, $alamat);
+            // $stmt->bindValue(3, $no_hp);
+            // $stmt->execute();
 
-            echo "window.location.href = 'https://azurewebaoorifki02.azurewebsites.net/';";
+            // echo "window.location.href = 'https://azurewebaoorifki02.azurewebsites.net/';";
 
  ?>
