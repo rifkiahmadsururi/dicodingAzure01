a87bba0431e5617687e112ea784da173e9a7678d
rifkiahmadsururi <rifki.sururi27@gmail.com>
2019-04-11 18:55:02 +0700

    Add files via upload

--- tambahaction.php
+++ tambahaction.php
@@ -1,5 +1,5 @@
 <?php 
-            //include 'koneksi.php';
+            include 'koneksi.php';
             $nama = $_POST['nama'];
             echo $nama;
             $alamat = $_POST['alamat'];
