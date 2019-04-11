f960b54ee687be3677aa672949a7258a9be6dea2
rifkiahmadsururi <rifki.sururi27@gmail.com>
2019-04-11 19:17:39 +0700

    Add files via upload

--- tambahaction.php
+++ tambahaction.php
@@ -4,11 +4,8 @@
         $nama = $_POST['nama'];
         $alamat = $_POST['alamat'];
         $no_hp = $_POST['no_hp'];
-            // // Insert data
         $sql_insert = "exec sp_insertMahasiswa $nama, $alamat, $no_hp";
         $stmt = $conn->prepare($sql_insert);
         $stmt->execute();
    
  ?>
-
- <a href="https://azurewebaoorifki02.azurewebsites.net"></a><button>Home</button></a>
