5ba1a2333b6d71d0579a38e7af7f2721a0e17753
rifkiahmadsururi <rifki.sururi27@gmail.com>
2019-04-11 19:13:00 +0700

    Add files via upload

--- tambahaction.php
+++ tambahaction.php
@@ -1,15 +1,16 @@
 <?php 
-            include 'koneksi.php';
-
-            $nama = $_POST['nama'];
-            
-            $alamat = $_POST['alamat'];
-            $no_hp = $_POST['no_hp'];
+    try {
+        include 'koneksi.php';
+        $nama = $_POST['nama'];
+        $alamat = $_POST['alamat'];
+        $no_hp = $_POST['no_hp'];
             // // Insert data
-            $sql_insert = "exec sp_insertMahasiswa $nama, $alamat, $no_hp";
-            $stmt = $conn->prepare($sql_insert);
-            $stmt->execute();
-
-            // echo "window.location.href = 'https://azurewebaoorifki02.azurewebsites.net/';";
+        $sql_insert = "exec sp_insertMahasiswa $nama, $alamat, $no_hp";
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->execute();
+        echo "window.location.href = 'https://azurewebaoorifki02.azurewebsites.net/';";
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
 
  ?>
