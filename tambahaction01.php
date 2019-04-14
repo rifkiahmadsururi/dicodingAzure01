4a37d5e2a216ec439917714c2822454660b98bfa
rifkiahmadsururi <rifki.sururi27@gmail.com>
2019-04-14 10:34:10 +0700

    Add files via upload

--- tambahaction01.php
+++ tambahaction01.php
@@ -5,7 +5,8 @@
         //$alamat = $_POST['alamat'];
         //$hobi = $_POST['hobi'];
         //$sql_insert = "INSERT INTO mahasisiwa VALUES (nama, alamat, hobi) ($nama, $alamat, $hobi, '1999-09-27')";
-        $sql_insert = "INSERT INTO `azureSqlServerRifki01`.`mahasisiwa` VALUES (nama, alamat, hobi, tanggal) ('rifki ahmad sururi', 'bandung', 'menulis', '1999-09-27')";
+        $nama = 'rifki';
+        $sql_insert = "INSERT INTO `azureSqlServerRifki01`.`mahasisiwa` VALUES (nama, alamat, hobi, tanggal) ('".$nama."', 'bandung', 'menulis', '1999-09-27')";
         $stmt = $conn->prepare($sql_insert);
         $stmt->execute();
    
